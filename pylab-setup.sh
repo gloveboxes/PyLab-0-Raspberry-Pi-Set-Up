857bdf35b7bd4c9bdeed7c886d1dc123674025aa
Dave Glover <dglover@microsoft.com>
2019-09-11 08:09:25 +1000

    doco

--- pylab-setup.sh
+++ pylab-setup.sh
@@ -2,7 +2,10 @@
 
 sudo apt update && sudo apt install -y git
 
-rm -r -f ~/PyLab && \
-git clone --depth=1 https://github.com/gloveboxes/PyLab-0-Raspberry-Pi-Set-Up.git ~/PyLab && \
-sudo chmod +x ~/PyLab/Lab-setup-multiuser/setup/*.sh && \
+rm -r -f ~/PyLab
+
+git clone --depth=1 https://github.com/gloveboxes/PyLab-0-Raspberry-Pi-Set-Up.git ~/PyLab
+
+sudo chmod +x ~/PyLab/Lab-setup-multiuser/setup/*.sh
+
 cd ~/PyLab/Lab-setup-multiuser/setup
