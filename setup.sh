e03f1c56fe33712c02b14433733faff7906e04b8
Dave Glover <dglover@microsoft.com>
2019-09-11 09:07:29 +1000

    doco

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# curl https://raw.githubusercontent.com/gloveboxes/PyLab-0-Raspberry-Pi-Set-Up/master/setup.sh | bash
-export terminal=$(tty)
+# bash -c "$(curl https://raw.githubusercontent.com/gloveboxes/PyLab-0-Raspberry-Pi-Set-Up/master/setup.sh)"
 
-# sudo apt update && sudo apt install -y git
+sudo apt install -y git
 
 rm -r -f ~/PyLab
 echo 'Git Cloning the Setup Libraries'
@@ -11,8 +10,6 @@ git clone --depth=1 https://github.com/gloveboxes/PyLab-0-Raspberry-Pi-Set-Up.gi
 
 sudo chmod +x ~/PyLab/setup-multiuser/*.sh
 sudo chmod +x ~/PyLab/setup-singleuser/*.sh
-
-read -p "Single or Multi user set up? ([S]ingle, [M]ulti), [Q]uit: " PyLab_Setup_Mode < $terminal
 
 while true; do
     read -p "Single or Multi user set up? ([S]ingle, [M]ulti), [Q]uit: " PyLab_Setup_Mode
@@ -22,7 +19,7 @@ while true; do
         [Qq]* ) exit 1;;
         * ) echo "Please answer yes(y), no(n), or quit(q).";;
     esac
-done  < $terminal
+done
 
 if [ $PyLab_Setup_Mode = 'S' ] || [ $PyLab_Setup_Mode = 's' ]; then   
     echo 'single mode'
