c413469ed700b12b97058146600763d354455fc8
Dave Glover <dglover@microsoft.com>
2019-09-11 17:22:32 +1000

    doco

--- setup-openwrt/20-openwrt-refresh-PyLab.sh
+++ setup-openwrt/20-openwrt-refresh-PyLab.sh
@@ -1,5 +1,7 @@
-INSTALL_DIR=/mnt/sda2 && \
-rm -r -f $INSTALL_DIR/PyLab && \
-git clone --depth=1 git://github.com/gloveboxes/PyLab-0-Raspberry-Pi-Set-Up.git $INSTALL_DIR/PyLab && \
-chmod +x $INSTALL_DIR/PyLab/setup-openwrt/*.sh && \
-cd $INSTALL_DIR/PyLab/setup-openwrt/
+INSTALL_DIR=/mnt/sda2
+
+rm -r -f $INSTALL_DIR/PyLab
+git clone --depth=1 git://github.com/gloveboxes/PyLab-0-Raspberry-Pi-Set-Up.git $INSTALL_DIR/PyLab
+
+chmod +x $INSTALL_DIR/PyLab/setup-openwrt/*.sh
+$INSTALL_DIR/PyLab/setup-openwrt/21-openwrt-load-ftp.sh
---
+++ setup-openwrt/README.md
@@ -0,0 +1,5 @@
+# OpenWRT Bootstrap
+
+```bash
+bash -c "$(curl https://raw.githubusercontent.com/gloveboxes/PyLab-0-Raspberry-Pi-Set-Up/master/setup-openwrt/20-openwrt-refresh-PyLab.sh)"
+```
