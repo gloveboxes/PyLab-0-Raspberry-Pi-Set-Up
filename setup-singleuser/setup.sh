b022a390ff43f87f71ee348008ae3439ef16d60b
Dave Glover <dglover@microsoft.com>
2019-09-15 09:46:39 +1000

    doco

--- setup-singleuser/setup.sh
+++ setup-singleuser/setup.sh
@@ -11,3 +11,10 @@ $LAB_DIR/12-load-ftp.sh
 $LAB_DIR/14-create-users.sh
 $LAB_DIR/15-copy-lab.sh
 $LAB_DIR/16-build-images.sh
+
+echo
+echo
+echo "The Raspberry Pi will now shutdown."
+echo "Remove Power and attach the Pi Sense HAT."
+echo "Remember when you restart your Raspberry Pi that you renamed it."
+sudo halt
