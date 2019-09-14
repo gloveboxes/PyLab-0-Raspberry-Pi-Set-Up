06de0ef9e0867fd50dcf3e7f2dd35597c7163a46
Dave Glover <dglover@microsoft.com>
2019-09-14 17:34:34 +1000

    doco

--- scripts/windows-ssh-setup.cmd
+++ scripts/windows-ssh-setup.cmd
@@ -22,35 +22,28 @@ if ERRORLEVEL 1 (
   rem https://www.tenforums.com/tutorials/23975-find-windows-10-build-number.html
 
   if %version% GEQ 10 if %build% GEQ 17134 (
-
-    echo(
-    echo ===========================================================
-    echo ERROR: NO SSH SUPPORT FOUND
-    echo Version of Windows 10 1803 or better is installed
-    echo Install OpenSSH Client https://docs.microsoft.com/windows-server/administration/openssh/openssh_install_firstuse
-    echo Install: From Windows 10 Settings, Search Manage optional features, Add OpenSSH Client
-    echo ===========================================================
-
+	echo(
+	echo ===========================================================
+	echo ERROR: NO SSH SUPPORT FOUND
+	echo Version of Windows 10 1803 or better is installed
+	echo Install OpenSSH Client https://docs.microsoft.com/windows-server/administration/openssh/openssh_install_firstuse
+	echo Install: From Windows 10 Settings, Search Manage optional features, Add OpenSSH Client
+	echo ===========================================================
   ) else (
-
     where git /Q
 
     if ERRORLEVEL 1 (
-      echo(
-      echo ======================================================================================
-      echo ERROR: NO SSH SUPPORT FOUND
-      echo Version of Windows installed is older than Windows 10 1803.
-      echo Install Git Client from https://git-scm.com/download/win and rerun the windows-ssh.cmd
-      echo ======================================================================================
-      echo(
-
+		echo(
+		echo ======================================================================================
+		echo ERROR: NO SSH SUPPORT FOUND
+		echo Version of Windows installed is older than Windows 10 1803.
+		echo Install Git Client from https://git-scm.com/download/win and rerun the windows-ssh.cmd
+		echo ======================================================================================
+		echo(
     ) else (
-
-      set usegit="true"
-      GOTO :start
-
+		set usegit="true"
+		GOTO :start
     )
-
   )
 
   goto :exit
@@ -67,69 +60,47 @@ if ERRORLEVEL 1 (
     echo(
 
     if %version% GEQ 10 if %build% GEQ 16257 (
-
-      set /P c="Raspberry Pi Network Address [101;93m%PYLAB_IPADDRESS%[0m, login name [101;93m%PYLAB_LOGIN%[0m. Correct? ([Y]es,[N]o,[Q]uit): "
-
+		set /P c="Raspberry Pi Network Address [101;93m%PYLAB_IPADDRESS%[0m, login name [101;93m%PYLAB_LOGIN%[0m. Correct? ([Y]es,[N]o,[Q]uit): "
     ) else (
-
-      set /P c="Raspberry Pi Network entered was '%PYLAB_IPADDRESS%', login name '%PYLAB_LOGIN%'.  Correct? ([Y]es/[N]o): "
-
+		set /P c="Raspberry Pi Network entered was '%PYLAB_IPADDRESS%', login name '%PYLAB_LOGIN%'.  Correct? ([Y]es/[N]o): "
     )
 
     if /I "%c%" EQU "Y" goto :updateconfig
     if /I "%c%" EQU "N" goto :start
+	
     echo (
     echo Rerun windows-ssh.cmd
+	
     goto :exit
 
 :updateconfig
 
-    if not exist %USERPROFILE%\.ssh\NUL mkdir %USERPROFILE%\.ssh
-
-    set PYLAB_SSHCONFIG=%USERPROFILE%\.ssh\config
-    set PYLAB_TIME=time /T
-
-    echo(
-    echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    echo Updating SSH Config file %USERPROFILE%\.ssh\config
-    echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    echo(
-
-    echo( >> %PYLAB_SSHCONFIG%
-    echo #Begin-PyLab %DATE% >> %PYLAB_SSHCONFIG%
-    echo Host pylab-%PYLAB_LOGIN% >> %PYLAB_SSHCONFIG%
-    echo     HostName %PYLAB_IPADDRESS% >> %PYLAB_SSHCONFIG%
-    echo     User %PYLAB_LOGIN% >> %PYLAB_SSHCONFIG%
-    echo     IdentityFile ~/.ssh/id_rsa_python_lab >> %PYLAB_SSHCONFIG%
-    echo #End-PyLab >> %PYLAB_SSHCONFIG%
-    echo( >> %PYLAB_SSHCONFIG%
-
-    echo(
-    echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    echo Generating SSH Key file %USERPROFILE%\.ssh\id_rsa_python_lab
-    echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    echo(
-
-    rem Next command does not work inside a conditional block
-    for /F "tokens=* USEBACKQ" %%F in (`where git`) do ( set gitpath=%%F ) 2>nul
-
-    if %usegit% == "true" (
-
-      echo Generating SSH Key with Git SSH
-
-      "%gitpath:~0,-13%\git-bash.exe" -c "ssh-keygen -t rsa -N '' -b 4096 -f ~/.ssh/id_rsa_python_lab"
-    
-    ) else (
-
-      echo Generating SSH Key Windows SSH Client
-
-      ssh-keygen -t rsa -b 4096 -N "" -f %USERPROFILE%\.ssh\id_rsa_python_lab
-
-    )
-
-    set REMOTEHOST=%PYLAB_LOGIN%@%PYLAB_IPADDRESS%
     set PATHOFIDENTITYFILE=%USERPROFILE%\.ssh\id_rsa_python_lab.pub
+
+    if not exist %USERPROFILE%\.ssh\NUL mkdir %USERPROFILE%\.ssh
+	
+	if not exist %PATHOFIDENTITYFILE% (
+		echo(
+		echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		echo Generating SSH Key file %USERPROFILE%\.ssh\id_rsa_python_lab
+		echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		echo(
+
+		rem Next command does not work inside a conditional block
+		for /F "tokens=* USEBACKQ" %%F in (`where git`) do ( set gitpath=%%F ) 2>nul
+
+		if %usegit% == "true" (
+		  echo Generating SSH Key with Git SSH
+		  "%gitpath:~0,-13%\git-bash.exe" -c "ssh-keygen -t rsa -N '' -b 4096 -f ~/.ssh/id_rsa_python_lab"
+		) else (
+		  echo Generating SSH Key Windows SSH Client
+		  ssh-keygen -t rsa -b 4096 -N "" -f %PATHOFIDENTITYFILE%
+		)	
+	)
+
     set /p PYLAB_KEY=<%USERPROFILE%\.ssh\id_rsa_python_lab.pub
+	set PATHOFIDENTITYFILE=%USERPROFILE%\.ssh\id_rsa_python_lab.pub
+	set REMOTEHOST=%PYLAB_LOGIN%@%PYLAB_IPADDRESS%
 
     echo(
     echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -138,40 +109,76 @@ if ERRORLEVEL 1 (
 
     rem https://gist.github.com/mlocati/fdabcaeb8071d5c75a2d51712db24011
     if %version% GEQ 10 if %build% GEQ 16257 (
-
-      echo [101;93mAccept continue connecting: type yes[0m
-      echo [101;93mThe Raspberry Pi Password is raspberry[0m
-      echo(
-      echo [101;93mThe password will NOT display as you type it[0m
-      echo [101;93mWhen you have typed the password press ENTER[0m
-      echo [101;93mYou may need to press ENTER twice[0m
-
-    ) else (
-      
-      echo Accept continue connecting: type yes
-      echo The Raspberry Pi Password is raspberry
-      echo(
-      echo The password will NOT display as you type it
-      echo When you have typed the password press ENTER
-      echo You may need to press ENTER twice
-
+		echo [101;93mAccept continue connecting: type yes[0m
+		echo [101;93mThe Raspberry Pi Password is raspberry[0m
+		echo(
+		echo [101;93mThe password will NOT display as you type it[0m
+		echo(
+		echo [101;93mWhen you have typed the password press ENTER[0m
+		echo [101;93mYou may need to press ENTER TWICE[0m
+    ) else (      
+		echo Accept continue connecting: type yes
+		echo The Raspberry Pi Password is raspberry
+		echo(
+		echo The password will NOT display as you type it
+		echo(
+		echo When you have typed the password press ENTER
+		echo You may need to press ENTER TWICE
     )
+	
     echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     echo(
+	
+	setlocal enabledelayedexpansion
 
     if %usegit% == "true" (
-
-      "%gitpath:~0,-13%\git-bash.exe" -c "ssh %REMOTEHOST% 'mkdir -p ~/.ssh && chmod 700 ~/.ssh && echo %PYLAB_KEY% >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys'"
-
+		"%gitpath:~0,-13%\git-bash.exe" -c "ssh %REMOTEHOST% 'mkdir -p ~/.ssh && chmod 700 ~/.ssh && echo %PYLAB_KEY% >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys'"
     ) else (
+		ssh %REMOTEHOST% "mkdir -p ~/.ssh && chmod 700 ~/.ssh && echo '%PYLAB_KEY%' >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys" 
+	)	
+	
+	set error=!errorlevel!
+    set PYLAB_SSHCONFIG=%USERPROFILE%\.ssh\config
 
-      ssh %REMOTEHOST% "mkdir -p ~/.ssh && chmod 700 ~/.ssh && echo '%PYLAB_KEY%' >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys" 
-    )
+    if %error% EQU 0 (
+		echo(
+		echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		echo Updating SSH Config file %USERPROFILE%\.ssh\config
+		echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		echo(
+
+		echo( >> %PYLAB_SSHCONFIG%
+		echo #Begin-PyLab %DATE% >> %PYLAB_SSHCONFIG%
+		echo Host pylab-%PYLAB_LOGIN% >> %PYLAB_SSHCONFIG%
+		echo     HostName %PYLAB_IPADDRESS% >> %PYLAB_SSHCONFIG%
+		echo     User %PYLAB_LOGIN% >> %PYLAB_SSHCONFIG%
+		echo     IdentityFile ~/.ssh/id_rsa_python_lab >> %PYLAB_SSHCONFIG%
+		echo #End-PyLab >> %PYLAB_SSHCONFIG%
+		echo( >> %PYLAB_SSHCONFIG%
+		
+		if %version% GEQ 10 if %build% GEQ 16257 (
+			echo [101;93mSuccessfully created, copied SSH Key to Raspberry Pi, and set up %USERPROFILE%\.ssh\config[0m
+		) else (		
+			echo Successfully created, copied SSH Key to Raspberry Pi, and set up %USERPROFILE%\.ssh\config
+		)	
+	) else (	
+		echo(
+	
+		if %version% GEQ 10 if %build% GEQ 16257 (
+			echo [101;93mThe SSH Public key was NOT copyied to the Raspberry Pi.[0m
+			echo [101;93mCheck Raspberry Pi Network address and password and try again.[0m
+		) else (
+			echo The SSH Public key was NOT copyied to the Raspberry Pi.
+			echo Check Raspberry Pi Network address and password and try again.		  
+		)		
+	)
 
 :exit
 
     echo(
     echo Finished
     echo(
+	
+	endlocal
 
 pause
