ce29b275a99a2a380f8a7cb8801b3c94451a7447
Dave Glover <dglover@microsoft.com>
2019-09-27 11:44:02 +1000

    doco

--- setup-multiuser/setup.sh
+++ setup-multiuser/setup.sh
@@ -6,18 +6,18 @@ sed --in-place '/~\/PyLab\/setup-multiuser\/10-Install-prerequisites.sh/d' ~/.ba
 
 LAB_DIR=~/PyLab/setup-multiuser
 
-$LAB_DIR/10-Install-prerequisites.sh
+($LAB_DIR/10-Install-prerequisites.sh)
 if [ $? -ne 0 ]; then
     rm ~/.PyLabState
     exit 1
 fi
 
-$LAB_DIR/11-install-core.sh
-$LAB_DIR/12-load-ftp.sh
-$LAB_DIR/13-load-vs-code.sh
-$LAB_DIR/14-create-users.sh
-$LAB_DIR/15-copy-lab.sh
-$LAB_DIR/16-build-images.sh
+($LAB_DIR/11-install-core.sh)
+($LAB_DIR/12-load-ftp.sh)
+($LAB_DIR/13-load-vs-code.sh)
+($LAB_DIR/14-create-users.sh)
+($LAB_DIR/15-copy-lab.sh)
+($LAB_DIR/16-build-images.sh)
 # $LAB_DIR/17-copy-remote-ssh.sh
 
 rm ~/.PyLabState
