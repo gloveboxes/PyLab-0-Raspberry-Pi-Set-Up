64a5042c18f1a94c457c159ea8e16f2fb829b68b
Dave Glover (DEVELOPER RELATIONS) <dglover@microsoft.com>
2019-12-06 10:13:02 +1100

    doco

--- setup/scriptlets/common/setup-shutdown.sh
+++ setup/scriptlets/common/setup-shutdown.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo sed -i -e '$i \sudo python3 ~/PyLab-0-Raspberry-Pi-Set-Up-master/setup/shutdown/shutdown.py &\n' /etc/rc.local
+sudo sed -i -e '$i \python3 /home/pi/PyLab-0-Raspberry-Pi-Set-Up-master/setup/shutdown/shutdown.py &\n' /etc/rc.local
